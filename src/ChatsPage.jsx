1854bcffaf48421be00c4ad2f817106044bfeceb
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-11-03 16:57:00 -0500

    preuba

--- src/ChatsPage.jsx
+++ src/ChatsPage.jsx
@@ -4,7 +4,7 @@ const ChatsPage = (props) => {
   return (
     <div className="background">
       <PrettyChatWindow
-        projectId={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID}
+        projectId={"f9ec984e-8c28-4275-be75-34a5c5b2ede5"}
         username={props.user.username}
         secret={props.user.secret}
       />
