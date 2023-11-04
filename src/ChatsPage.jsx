f04ca2ecdd20ee8586febbe74268cd7fafcc6948
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-11-03 21:19:28 -0500

    prueba

--- src/ChatsPage.jsx
+++ src/ChatsPage.jsx
@@ -1,14 +1,13 @@
-import { PrettyChatWindow } from "react-chat-engine-pretty";
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic} from "react-chat-engine-advanced";
 
 const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic('f9ec984e-8c28-4275-be75-34a5c5b2ede5', props.user.username, props.user.secret)
   return (
-    <div className="background">
-      <PrettyChatWindow
-        projectId={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID}
-        username={props.user.username}
-        secret={props.user.secret}
-      />
+    <div style={{height: '100vh'}}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{height: '100%'}} />
     </div>
+    
   );
 };
 
